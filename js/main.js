30de09ef1a03e705e1c353e5d886ec76a4b342ef
johnworker <besscomdark@gmail.com>
2022-02-27 20:01:17 +0800

    添加內容

---
+++ js/main.js
@@ -0,0 +1,50 @@
+/****************************************************************
+ *																*		
+ * 						      �����							*
+ *                        www.dmaku.com							*
+ *       		  Ŭ���������ơ��������²���Լ�ģ��			*
+ * 																*
+****************************************************************/
+$(document).ready(function($){
+
+    var crotation;
+    var rotateto = 0;
+    var itemCount = $('item').length; // count of items in corousel
+    var tcItemInitialRotation = 360/itemCount;
+    var tcZDistance = 240;
+    
+    $('item').each( function(index) {
+                   
+        $(this).css({
+            'transform' : 'rotateY('+( tcItemInitialRotation * index )+'deg) translateZ('+tcZDistance+'px)'
+        }).attr('tc-rotation', ( tcItemInitialRotation * index ) );
+
+    });
+    
+    function tcRotate(tcdeg){
+        $('#container').css({
+            'transform'         : 'rotateY('+ tcdeg +'deg)',
+            '-ms-transform'     : 'rotateY('+ tcdeg +'deg)',
+            '-webkit-transform' : 'rotateY('+ tcdeg +'deg)'
+        });
+    }
+    
+    $('item').on('click', function(){
+        crotation = $('#container').attr('tcc-rotation');
+        var rotation = $(this).attr('tc-rotation');       
+        rotateto = crotation - rotation;
+        tcRotate(rotateto);
+        crotation = rotateto;
+    });
+    
+    $('.tc-next').on('click', function(){
+        rotateto -= tcItemInitialRotation;
+        tcRotate(rotateto);
+    });
+    $('.tc-prev').on('click', function(){
+        rotateto += tcItemInitialRotation;
+        tcRotate(rotateto);
+    });
+
+});
+console.log("\u002f\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u000d\u000a\u0020\u002a\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u0009\u0009\u000d\u000a\u0020\u002a\u0020\u0009\u0009\u0009\u0009\u0009\u0009\u0020\u0020\u0020\u0020\u0020\u0020\u4ee3\u7801\u5e93\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u000d\u000a\u0020\u002a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0077\u0077\u0077\u002e\u0064\u006d\u0061\u006b\u0075\u002e\u0063\u006f\u006d\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u000d\u000a\u0020\u002a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0009\u0009\u0020\u0020\u52aa\u529b\u521b\u5efa\u5b8c\u5584\u3001\u6301\u7eed\u66f4\u65b0\u63d2\u4ef6\u4ee5\u53ca\u6a21\u677f\u0009\u0009\u0009\u002a\u000d\u000a\u0020\u002a\u0020\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u000d\u000a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002f");
