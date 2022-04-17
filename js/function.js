a908d8e3d4f5cdc53f53e6f729ce5b7de4e2cd09
johnworker <besscomdark@gmail.com>
2022-04-17 19:31:27 +0800

    添加內容

--- js/function.js
+++ js/function.js
@@ -1,11 +1,26 @@
-$(".box_one").hover(0,function(){
-    $(".p_one").show(500);
+$(".box_one").on({
+    mouseenter: function () {
+        $(".p_one").show(500);
+    },
+    mouseleave: function () {
+        $(".p_one").hide(500);
+    }
 });
 
-$(".box_two").hover(0,function(){
-    $(".p_two").show(500);
+$(".box_two").on({
+    mouseenter: function () {
+        $(".p_two").show(500);
+    },
+    mouseleave: function () {
+        $(".p_two").hide(500);
+    }
 });
 
-$(".box_three").hover(0,function(){
-    $(".p_three").show(500);
+$(".box_three").on({
+    mouseenter: function () {
+        $(".p_three").show(500);
+    },
+    mouseleave: function () {
+        $(".p_three").hide(500);
+    }
 });
