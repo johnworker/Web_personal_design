deae2ad25daa1119719769897484f9a5fcbf87fb
johnworker <besscomdark@gmail.com>
2022-04-17 17:11:00 +0800

    樣式修改

---
+++ js/function.js
@@ -0,0 +1,11 @@
+$(".box_one").hover(0,function(){
+    $(".p_one").show(500);
+});
+
+$(".box_two").hover(0,function(){
+    $(".p_two").show(500);
+});
+
+$(".box_three").hover(0,function(){
+    $(".p_three").show(500);
+});
