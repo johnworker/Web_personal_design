d80c4607029a68877effb7851d93e720507ad3d5
johnworker <besscomdark@gmail.com>
2022-05-21 11:47:05 +0800

    樣式修改

--- js/slick.js
+++ js/slick.js
@@ -1,7 +1,33 @@
-    /*slick*/
-    $(".slick_track").slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 500,
-      });
+// slick響應式
+$(".slick").slick({
+
+  // the magic
+  responsive: [{
+
+    breakpoint: 1680,
+    settings: {
+      autoplay: true,
+      autoplaySpeed: 500,
+      vertical: true,
+      slidesToShow: 3,
+      pauseOnHover: false,
+      pauseOnFocus: false,
+      mobileFirst: true,
+      adaptiveHeight: true
+    }
+
+  }, {
+
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+      dots: true
+    }
+
+  }, {
+
+    breakpoint: 300,
+    settings: "unslick" // destroys slick
+
+  }]
+});
